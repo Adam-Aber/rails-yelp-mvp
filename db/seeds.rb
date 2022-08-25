46e5d818681630ce12d17e9c8bb95a98fc7d079f
Adam Aberbach <aberbach2023@gmail.com>
2022-08-25 11:20:06 +0100

    finished yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+phone_number = ["0625489758", "0658748251", "0632147801", "0658748127", "0621412314", "0677412580", "0625588143"]
+7.times {Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: categories.sample, phone_number: phone_number.sample)}
